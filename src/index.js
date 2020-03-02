406627e93b4c1f8519b527f4c3c4954e1566bd88
Joaquim <joaquimmy@yahoo.com>
2020-03-01 22:37:34 -0300

    Add choice prompter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const message = require('./prompters/message');
 const confirm = require('./prompters/confirm');
+const choice = require('./prompters/choice');
 
 module.exports = {
   message,
   confirm,
+  choice,
 };
---
+++ src/prompters/choice.js
@@ -0,0 +1,78 @@
+const makeFilter = (choices, userId) => (reaction, user) => {
+  const correctUser = (!userId || user.id === userId) && !user.bot;
+  return correctUser && (choices.includes(reaction.emoji.name) || choices.includes(reaction.emoji));
+};
+
+const choice = (
+  {
+    channel,
+    question = 'Pick one',
+    choices = ['💓', '💙'],
+    userId = '',
+    timeout = 30000,
+    deleteMessage = true,
+    acceptEarly = false,
+  } = {
+    question: 'Pick one',
+    choices: ['💓', '💙'],
+    userId: '',
+    timeout: 30000,
+    deleteMessage: true,
+    acceptEarly: false,
+  },
+) => {
+  if (!channel) throw new Error('Missing channel');
+  if (!choices) {
+    throw new Error('Choice prompt requires emoji choices');
+  }
+  if (!timeout) timeout = 30000;
+  if (deleteMessage === undefined) deleteMessage = true;
+  if (!acceptEarly) acceptEarly = false;
+
+  const getResponse = async () => {
+    const msg = await channel.send(question);
+    const message = msg instanceof Array ? msg[0] : msg;
+
+    // React with possible choices
+    for (const choice of choices) {
+      if (!acceptEarly) {
+        await message.react(choice);
+      } else {
+        message.react(choice);
+      }
+    }
+
+    // Await the response
+    let collected;
+    try {
+      collected = await message.awaitReactions(makeFilter(choices, userId), {
+        time: timeout,
+        max: 1,
+        errors: ['time'],
+      });
+    } catch (e) {
+      // If time ran out, delete message and resolve
+      if (deleteMessage) await message.delete();
+      return null;
+    }
+
+    if (deleteMessage) await message.delete();
+
+    let result = null;
+
+    for (const reaction of collected) {
+      const guildEmoji = message.guild ? message.guild.emojis.get(reaction[0]) : null;
+      result = guildEmoji ? guildEmoji : reaction[0];
+    }
+
+    return result;
+  };
+
+  return new Promise((resolve, reject) => {
+    getResponse()
+      .then(resolve)
+      .catch(reject);
+  });
+};
+
+module.exports = choice;
